4a5d1abc5896775b96f01a68122c9df430f911e3
kuldeepsharma1 <hytek112@gmail.com>
2026-05-28 06:28:48 +0530

    breadcrumbs fix

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -5,7 +5,7 @@ import type { BreadcrumbItem } from '@/types';
 
 
 export default function AppLayout({
-    breadcrumbs = [],
+    breadcrumbs,
     children,
 }: {
     breadcrumbs?: BreadcrumbItem[];
